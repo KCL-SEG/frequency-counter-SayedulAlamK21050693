8024f34ce1e1acdd96e90173ac1cd0af9e880c82
sayedul alam <sayedul.alam@kcl.ac.uk>
2022-12-09 02:56:12 +0000

    completed task

--- frequencies.py
+++ frequencies.py
@@ -4,4 +4,11 @@
 def frequencies(items):
     frequencies = {}
     # Your code goes here
+    for item in items:
+        i = str(item)
+        if i in frequencies.keys():
+            frequencies[i] += 1
+        else:
+            frequencies[i] = 1
+
     return frequencies
